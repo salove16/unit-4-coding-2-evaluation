e7a370aba955558e6d537eb16827b10a699a148e
salove16 <shashank.shalabh909@gmail.com>
2022-03-14 13:30:10 +0530

    gh

--- src/controller/fixedAccountController.js
+++ src/controller/fixedAccountController.js
@@ -1,13 +1,13 @@
 const express=require("express")
 
-const SavingAccount=require("../models/savingAccountModel")
+const FixedAccount=require("../models/fixedAccountModel")
 
 const router=express=express.Router()
 
 router.post("/",async(req,res)=>{
     try {
-        const savingAccount=await SavingAccount.create(req.body)
-return res.status(200).send(savingAccount)
+        const fixedAccount=await FixedAccount.create(req.body)
+return res.status(200).send(fixedAccount)
 
     } catch (err) {
         console.log(err)
